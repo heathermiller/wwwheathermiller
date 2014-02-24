c4c95a836db28ec26847e99d4ab0695600bacb85
Heather Miller <heather.miller@epfl.ch>
2014-02-25 00:35:27 +0100

    Getting viewcounts from the rest of my speakerdeck talks

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,10 +2,13 @@
 ---
 
 // speaker deck viewcounts
-var futuresPromises = getSpeakerdeckStats("6cd6688b9e11a1f5e1dd19d711e87b98", "futures-promises-views");
+var futuresPromises = getSpeakerdeckStats("cv96bzz6", "futures-promises-views");
+var picklesSporesScaladays = getSpeakerdeckStats("5gabjggm", "pickles-spores-scaladays");
+var sporesStrangeloop = getSpeakerdeckStats("d350fdec", "spores-strangeloop");
+var instantPicklesOopsla = getSpeakerdeckStats("8qzwp7pc", "instant-pickles-oopsla");
 
-function getSpeakerdeckStats(apiKey, id) {
-  var url = "http://www.kimonolabs.com/api/cv96bzz6?apikey=" + apiKey;
+function getSpeakerdeckStats(key, id) {
+  var url = "http://www.kimonolabs.com/api/" + key + "?apikey=6cd6688b9e11a1f5e1dd19d711e87b98";
   $.ajax({
     "url": url,
     "crossDomain": true,
