74793d5d792fa5e5b32b48003a1f4829a526ba33
Heather Miller <heather.miller@epfl.ch>
2014-02-24 22:15:27 +0100

    Adding some js now

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,2 +1,31 @@
----
----
+// ---
+// ---
+
+/***************************
+ * Document initialization
+ **************************/
+$(document).ready(function(){
+
+// get info from speakerdeck
+function getViewcount(url) {
+  var json = $.ajax(url)
+    .done(function() {
+      alert( "success" );
+    })
+    .fail(function() {
+      alert( "error" );
+    })
+    .always(function() {
+      alert( "complete" );
+    });
+//   // var osname = "Unknown OS";
+//   // if (navigator.appVersion.indexOf("Win") != -1) osname = "Windows";
+//   // if (navigator.appVersion.indexOf("Mac") != -1) osname = "Mac OS";
+//   // if (navigator.appVersion.indexOf("Linux") != -1) osname = "Linux";
+//   // if (navigator.appVersion.indexOf("X11") != -1) osname = "UNIX";
+//   // return osname;
+}
+
+getViewcount("http://www.kimonolabs.com/api/cv96bzz6?apikey=6cd6688b9e11a1f5e1dd19d711e87b98");
+
+});
--- test-page-attempt.md
+++ test-page-attempt.md
@@ -1,10 +1,3 @@
 ---
 layout: front
-title: this is a test page!
 ---
-
-Oh yeah, I totally forgot the content
-
-## Test bump! woot!
-
-How about now?
